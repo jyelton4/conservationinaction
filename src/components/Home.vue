78498b1cca62a71f46854c3c83d3c3e502354027
JacobY <jyelton2040@gmail.com>
2019-11-03 09:50:44 +0000

    moved project into git repo

---
+++ src/components/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Conservation In Action</h1>
+    <p>Sharing acts of environmental advocacy from around the world.</p>
+    <DatePicker />
+  </div>
+</template>
+
+<script>
+// import DatePicker from 'ant-design-vue';
+// Vue.use(DatePicker);
+export default {
+  name: 'Home'
+}
+</script>
